8c0845770a666d3d58302d02a1a4e2c59239ddf4
Siddhant Shah <siddhantshah333@gmail.com>
2024-05-30 16:52:23 +0100

    k

---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import './Input.css';
+
+export default function Input() {
+    return (
+      <div align="center">
+        <h1>Type text to analyse:</h1>
+        <textarea rows="7">Type Here</textarea><br/>
+        <button type="button">Analyse</button>
+      </div>
+    );
+  }

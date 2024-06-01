41cbd146fe8b65ebde332bd80b7208e3f2ad72aa
Siddhant Shah <siddhantshah333@gmail.com>
2024-06-01 22:43:03 +0100

    SPRINT-1 removed comment statements

--- src/Input.js
+++ src/Input.js
@@ -32,14 +32,3 @@ function Input(){
 
 
 export default Input  ;
-
-// export default function Input() {
-//     return (
-//       <div align="center">
-//         <h1>Type text to analyse:</h1>
-//         <textarea rows="7">Type Here</textarea><br/>
-//         <button type="button">Analyse</button>
-//         <button type="button">Reset</button>
-//       </div>
-//     );
-//   }

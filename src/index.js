5ca6902f1ec6e016914e2870fd46f1039c86827c
Siddhant Shah <siddhantshah333@gmail.com>
2024-06-01 21:00:09 +0100

    SCRUM-1 removed unnecessary code and implement basic running project

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Input from './Input';
-import Output from './Output';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Input />
-    <Output />
   </React.StrictMode>
 );
 
